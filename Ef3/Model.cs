d76550095bcff70b6faa3452923392d1fce2511a
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2021-04-29 13:06:59 -0400

    Add project files.

---
+++ Ef3/Model.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ef3
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        // The following configures EF to create a Sqlite database file as `C:\blogging.db`.
+        // For Mac or Linux, change this to `/tmp/blogging.db` or any other absolute path.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=blogging.db");
+    }
+
+    public static class SystemConst
+    {
+        public const int MaxStringLengthInDb = 1000;
+    }
+    
+    public class Blog
+    {
+        // actual database fields
+        public int BlogId { get; set; }
+
+        [MaxLength(SystemConst.MaxStringLengthInDb)]
+        public string Title { get; set; }
+
+        [MaxLength(SystemConst.MaxStringLengthInDb)]
+        public string Log { get; set; }
+
+        [MaxLength(SystemConst.MaxStringLengthInDb)]
+        public string Url { get; set; }
+
+        // Navigation Property - Not a db field
+        public List<Post> Posts { get; } = new List<Post>();
+
+        public override string ToString()
+        {
+            return $"BlogId={BlogId}, " +
+                   $"Title=\"{Title.Length}\"" + Environment.NewLine +
+                   $"Log=\"{Log?.Length}\"" + Environment.NewLine +
+                   $"Url =\"{Url}\"" + Environment.NewLine +
+                   string.Join("\n", Posts);
+        }
+    }
+
+    public class Post
+    {
+        // actual database fields
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        // Foreign Key
+        public int BlogId { get; set; }
+
+        // Navigation Property - Not a db field
+        public Blog Blog { get; set; }
+
+        public override string ToString()
+        {
+            return $"\tPostId={PostId}\n\tTitle=\"{Title}\"\n\tContent =\"{Content}\"";
+        }
+    }
+}
+
---
+++ Ef3/Program.cs
@@ -0,0 +1,71 @@
+﻿//https://docs.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=visual-studio
+
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ef3
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            using (var db = new BloggingContext())
+            {
+                // Note: This sample requires the database to be created before running.
+
+                // Create
+                Console.WriteLine("Inserting a new blog");
+                var blog = new Blog
+                {
+                    Title = BigString(),
+                    Url = "http://blogs.msdn.com/adonet",
+                    Log = BigString()
+                };
+                db.Add(blog);
+                db.SaveChanges();
+
+                int blogCount = OutputAllBlogs(db);
+
+                Console.WriteLine("Records: " + blogCount);
+                //
+                // // Update
+                // Console.WriteLine("Updating the blog and adding a post");
+                // blog.Url = "https://devblogs.microsoft.com/dotnet";
+                // blog.Posts.Add(
+                //     new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+                // db.SaveChanges();
+
+                // Delete
+                // Console.WriteLine("Delete the blog");
+                // db.Remove(blog);
+                // db.SaveChanges();
+            }
+        }
+
+        private static string BigString()
+        {
+            string s ="";
+            for (int i = 0; i < 10_000; i++)
+                s += "a";
+            return s;
+        }
+
+        private static int OutputAllBlogs(BloggingContext db)
+        {
+            // Read
+            Console.WriteLine("Querying for a blog");
+            var blogs = db.Blogs
+                .Include(b => b.Posts)
+                .Where(b => b.Url.Contains("http"))
+                .OrderBy(b => b.BlogId); // converts LINQ into SQL
+
+            foreach (var b in blogs)
+            {
+                Console.WriteLine(b);
+            }
+
+            return blogs.Count();
+        }
+    }
+}
